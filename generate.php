c9213e3121040c9eb320a26feefe004188344629
Saddam Azy <myxzlpltk@gmail.com>
2021-05-03 14:52:39 +0700

    update rule

--- generate.php
+++ generate.php
@@ -6,6 +6,7 @@ $names = array_map(fn($hero) => $hero->getName(), (array)$app->heroes);
 
 $txt = new ArrayObject();
 foreach($app->heroes as $hero){
+    $txt->append("hero(\"{$hero->getName()}\").");
     foreach($hero->getEnemies() as $enemy){
         if(!in_array($enemy->getName(), $names)){
             echo "Something wrong: {$hero->getName()} {$enemy->getName()}";
@@ -20,4 +21,4 @@ file_put_contents("rule.txt", $data);
 echo "Total rule : ";
 echo count($txt);
 echo " ";
-echo count($names)**2-count($names) == count($txt) ? "MATCH" : "UNMATCH";
+echo count($names)**2 == count($txt) ? "MATCH" : "UNMATCH";
